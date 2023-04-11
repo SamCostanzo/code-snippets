e79018cc90c4448e7fb0faecb708eb4abf203c7e
Sam Costanzo <samcostanzo25@gmail.com>
2023-04-11 16:32:09 -0400

    Update index.php

--- index.php
+++ index.php
@@ -17,6 +17,21 @@
         } 
     }
 
+// Disable Block Editor on specific post types
+add_filter('use_block_editor_for_post_type', 'prefix_disable_gutenberg', 10, 2);
+function prefix_disable_gutenberg($current_status, $post_type){
+    // List post types here
+    if(
+		$post_type === 'product' ||
+	    $post_type === 'page' ||
+        $post_type === 'brew' ||
+        $post_type === 'promo-cards' ||
+        $post_type === 'slides'
+	   ) 
+	return false;
+    return $current_status;
+}
+
 // Shorthand if/else for displaying a custom field.
 //  "Get" that field, if it has a value, THEN (?) display it. Or ELSE (:) echo the string 'N/A'
 // (Don't forget an echo at the start)
